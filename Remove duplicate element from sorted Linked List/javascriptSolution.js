a558c70f76257c506c06c6081645af5fc6b1f4c9
amar-1999 <damarakeswarg1999@gmail.com>
2023-08-28 22:55:29 +0530

    Remove duplicate element from sorted Linked List

---
+++ Remove duplicate element from sorted Linked List/javascriptSolution.js
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function printlist(head) {
+    if (!head) return;
+    let current = head;
+    let s = '';
+    while (current !== null) {
+        s += current.data + " ";
+        current = current.next;
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let n = parseInt(readLine());
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for (let i = 1; i < n; i++) {
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let obj = new Solution();
+        let res = obj.removeDuplicates(head);
+        printlist(res);
+    }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    //Function to remove duplicates from sorted linked list.
+    removeDuplicates(head) {
+        //your code here
+        let current = head;
+
+        while (current !== null && current.next !== null) {
+            if (current.data === current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
